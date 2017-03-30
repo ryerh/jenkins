c20a8bd536855989b95907f4fde0f841d4261419
huangyan <huangyan@xsky.com>
2017-03-30 12:09:13 +0800

    Auto init submodule

--- build.sh
+++ build.sh
@@ -8,12 +8,12 @@ DOCKER_VERSION="17.03"
 JENKINS_BASE_IMAGE="jenkins:alpine"
 BUILD_IMAGE_NAME="jenkins-docker"
 
-if [[ -e docker/$DOCKER_VERSION ]]; then
-  cd docker/$DOCKER_VERSION
-else
-  echo "Invalid Dockerfile"
-  exit 1
+if [[ ! -e docker/$DOCKER_VERSION ]]; then
+  git submodule init
+  git submodule update
 fi
+
+cd docker/$DOCKER_VERSION
 
 sed "s/FROM .*/FROM $JENKINS_BASE_IMAGE/" Dockerfile | \
   docker build -t $BUILD_IMAGE_NAME -

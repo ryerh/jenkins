39ab6ef8b71301ede15b41925658b623b6bd75d9
huangyan <huangyan@xsky.com>
2017-03-30 14:53:06 +0800

    Add container names

--- start-simple.sh
+++ start-simple.sh
@@ -7,4 +7,5 @@ set -x
 docker run $@ \
   -p 8080:8080 \
   -v /var/run/docker.sock:/var/run/docker.sock \
+  --name jenkins-docker-simple
   jenkins-docker-simple
--- start.sh
+++ start.sh
@@ -7,5 +7,6 @@ set -x
 docker run $@ \
   -p 8080:8080 \
   -v /var/run/docker.sock:/var/run/docker.sock \
+  --name jenkins-docker-simple
   jenkins-docker \
   tini -- /usr/local/bin/jenkins.sh

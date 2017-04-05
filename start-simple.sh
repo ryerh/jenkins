aaa715bb4634a9f1470991a936b5e6a9a05370f5
huangyan <huangyan@xsky.com>
2017-04-05 12:14:34 +0800

    Backup jenkins home

--- start-simple.sh
+++ start-simple.sh
@@ -4,9 +4,12 @@
 #
 set -ex
 
+mkdir -p /var/jenkins_home
+
 docker run $@ \
   -it \
   -p 8080:8080 \
   -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /var/jenkins_home:/var/run/jenkins_home \
   --name jenkins-docker-simple \
   jenkins-docker-simple
